4c074ad18e6c926d353459ba36b0875be706eee3
siddhantprateek <siddhantprateek@gmail.com>
2023-08-22 13:37:01 +0530

    ft(config): config file optimzed

--- config/config.go
+++ config/config.go
@@ -7,44 +7,14 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func GetEnv() string {
+func GetEnv(ENV_VAR string) string {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Unable to Load environment variables")
 	}
-
-	OpenAI := os.Getenv("OPENAI_KEY")
-	if OpenAI == "" {
+	envVar := os.Getenv(ENV_VAR)
+	if envVar == "" {
 		log.Fatal("openai api key is not provided.")
 	}
-
-	return OpenAI
-}
-
-func GetCMCKey() string {
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Unable to load CMC API key")
-	}
-
-	cmc_api_key := os.Getenv("CMC_PRO_API_KEY")
-	if cmc_api_key == "" {
-		log.Fatal("cmc api key is not provided")
-	}
-
-	return cmc_api_key
-}
-
-func GetRapidAPI() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Unable to load rapid API key.")
-	}
-
-	rapidAPIKey := os.Getenv("RAPIDAPI_KEY")
-	if rapidAPIKey == "" {
-		log.Fatal("Please provide RAPIDAPI key to .env")
-	}
-	return rapidAPIKey
+	return envVar
 }
